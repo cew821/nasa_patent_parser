9b987f8f408de569e638a7964188fec588fa1786
Charles Worthington <c.e.worthington@gmail.com>
2014-10-29 09:50:02 -0400

    user can get json for a specific patent, and can get IDs for every patent

--- lib/parser.rb
+++ lib/parser.rb
@@ -19,23 +19,33 @@ def get_category_query_terms
 	category_query_terms
 end
 
-def parse_patent_ids_from_page(category_link, page = 0)
-	patent_ids = []
+def parse_patent_ids_from_page(category_link, page = 0, patent_ids = [])
 	uri = BASE_URI + "/svr/search.php?r=category&d=patent&q=#{category_link}&p=#{page.to_s}"
 	response = JSON.parse(open(uri).read)
 	if response["results"].count > 0
+		puts "Scraping IDs for page #{page}"
 		response["results"].each do |result|
 			patent_ids << result[4]
 		end
+		# page += 1
+		# parse_patent_ids_from_page(category_link, page, patent_ids)
 	end
 	patent_ids
 end
 
+def get_patent_details(patent_id)
+	uri = BASE_URI + "/svr/search.php?d=patent&r=geturl&q=#{patent_id}"
+	response = JSON.parse(open(uri).read)
+	response
+end
 
-# puts get_patent_links("materials and coatings")["results"][2]
 
-links = get_category_query_terms
-puts parse_patent_ids_from_page(links.first)
+patent = get_patent_details("patent_LAR-18143-1")["results"]
+puts patent["category"]
+puts patent["abstract"]
+
+# links = get_category_query_terms
+# puts parse_patent_ids_from_page(links.first)
 
 # get_categories
 # parse_page_of_links
